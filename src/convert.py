76fba7373ea8c5e8bdaab5d277c8abf39c3cb824
Sascha Brawer <sascha@brawer.ch>
2023-07-28 01:11:59 +0200

    Add public procurements of wsl.ch

--- src/convert.py
+++ src/convert.py
@@ -2,20 +2,34 @@ import csv
 import os.path
 
 
+SOURCES = {
+    'post_ch': (
+        'Die Schweizerische Post AG',
+        'Wankdorfallee 4',
+        '3030', 'Bern',
+        'CHE-109.030.864'),
+    'wsl_ch': (
+        'Eidgenössische Forschungsanstalt für Wald, Schnee und Landschaft',
+        'Zürcherstrasse 111',
+        '8903', 'Birmensdorf ZH',
+        'CHE-116.133.452'),
+}
+
+
 def read_csv(filename):
     path = os.path.join(os.path.dirname(__file__), 'data', filename)
     with open(path, 'r') as csv_file:
         for row in csv.DictReader(csv_file):
             yield row
 
-    
-def read_ch_post():
-    for row in read_csv('post_ch_2021.csv'):
-        print(row)
-        yield row
+
+def read_all():
+    for key in SOURCES.keys():
+        for row in read_csv(f'{key}_2021.csv'):
+            yield row
 
 
 if __name__ == '__main__':
-    for x in read_ch_post():
+    for x in read_all():
         print(x)
 
